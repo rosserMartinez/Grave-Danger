7b2c088a96522cee0276dbadad2043273b83b7a7
Rosser Martinez <rosser.martinez@gmail.com>
2017-09-13 00:27:22 -0400

    controller movement and button presses, no holes yet but im going with stationary holes for the sake of testing

---
+++ Gravelords/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class PlayerScript : MonoBehaviour {
+
+
+    Rigidbody2D rb;
+    public string LeftX;
+    public string LeftY;
+
+    public int moveSpeed;
+
+    // Use this for initialization
+    void Start () {
+        LeftX = "p1LeftX";
+        LeftY = "p1LeftY";
+
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 moveVec = new Vector2(Input.GetAxis(LeftX) * moveSpeed, -Input.GetAxis(LeftY) * moveSpeed);
+
+        rb.velocity = moveVec;
+
+        Debug.Log(Input.GetButton("p1A"));
+    }
+}
