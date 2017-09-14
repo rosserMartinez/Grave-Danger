fc8a27e4964d2337f24fe4d66ef3ae759b1eb27e
Cory <smithcory937@gmail.com>
2017-09-14 02:37:37 -0400

    gravedigging is in the game! ayy ready to show on thursday, but you can always further improve before showing
    future implementation ideas:
    ability to fall in graves unless you mash out
    highlight the currently nearby grave to signifiy that you can modify it

---
+++ Gravelords/Assets/Scripts/GraveScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveScript : MonoBehaviour {
+
+    SpriteRenderer rend;
+
+    CapsuleCollider2D cap;
+
+    public Sprite dugSprite;
+    public Sprite halfSprite;
+    public Sprite undugSprite;
+
+    public enum DigState { DUG, HALFDUG, UNDUG };
+
+    public DigState currentState;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        cap = GetComponentInChildren<CapsuleCollider2D>();
+
+        currentState = DigState.UNDUG;
+        updateGraveState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void updateGraveState()
+    {
+        if (currentState == DigState.DUG)
+        {
+            rend.sprite = dugSprite;
+            cap.enabled = true;
+        }
+        if (currentState == DigState.HALFDUG)
+        {
+            rend.sprite = halfSprite;
+            cap.enabled = false;
+        }
+        if (currentState == DigState.UNDUG)
+        {
+            rend.sprite = undugSprite;
+            cap.enabled = false;
+        }
+    }
+}
--- Gravelords/Assets/Scripts/PlayerScript.cs
+++ Gravelords/Assets/Scripts/PlayerScript.cs
@@ -3,30 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using XInputDotNetPure;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
 
-
+    BoxCollider2D playerCol;
     Rigidbody2D rb;
+    public GraveScript lastGrave;
+
     public string LeftX;
     public string LeftY;
 
     public int moveSpeed;
 
+    public bool canDig;
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         LeftX = "p1LeftX";
         LeftY = "p1LeftY";
 
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        playerCol = GetComponent<BoxCollider2D>();
+        lastGrave = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         Vector2 moveVec = new Vector2(Input.GetAxis(LeftX) * moveSpeed, -Input.GetAxis(LeftY) * moveSpeed);
 
         rb.velocity = moveVec;
 
-        Debug.Log(Input.GetButton("p1A"));
+        // Debug.Log(Input.GetButton("p1A"));
+
+        if (canDig)
+        {
+            Debug.Log(Input.GetButtonUp("p1A"));
+
+            if (Input.GetButtonUp("p1A") && lastGrave.currentState != GraveScript.DigState.DUG)
+            {
+
+                --lastGrave.currentState;
+
+                lastGrave.updateGraveState();
+            }
+            else if (Input.GetButtonUp("p1B") && lastGrave.currentState != GraveScript.DigState.UNDUG)
+            {
+                ++lastGrave.currentState;
+
+                lastGrave.updateGraveState();
+            }
+        }
+
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log(collision.GetComponentInParent<GraveScript>().currentState);
+
+        if (collision.tag == "digRange")
+        {
+            lastGrave = collision.GetComponentInParent<GraveScript>();
+
+            canDig = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "digRange")
+        {
+            lastGrave = null;
+
+            canDig = false;
+        }
+    }
+
 }
